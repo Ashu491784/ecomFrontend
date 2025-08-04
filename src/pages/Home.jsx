2aa64eedb2a3383f6cf079a609839d856bb22882
Ashu491784 <ayeshamadushani01@icloud.com>
2025-08-04 10:43:23 +0530

    image uploaded

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,148 +1,65 @@
+import { useEffect, useState } from "react";
+import { Button, AppBar, Toolbar, Typography } from "@mui/material";
+
 const Home = () => {
-  const ItemImage = ({ img }) => {
-    return <img src={img} className="card-img-top" alt="..."></img>;
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchItems = async () => {
+    try {
+      const res = await fetch("http://localhost:5000/api/item/getall");
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.message || "Failed to fetch items");
+      }
+
+      setItems(data.items);
+    } catch (err) {
+      console.error("Fetching error:", err);
+      alert("Error fetching items: " + err.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  const Title = ({ title }) => {
-    return <h5 className="card-title mt-2">{title}</h5>;
-  };
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
-  const Description = ({ desc }) => {
-    return <p className="card-text">{desc}</p>;
-  };
-
-  const Price = ({ price }) => {
-    return <h5 className="card-title">{price}</h5>;
-  };
-
-  const Button = ({ btnTitle, btnColor }) => {
-    return (
-      <a href="#" className={btnColor}>
-        {btnTitle}{" "}
-      </a>
-    );
-  };
-
-  const SearchForm = () => {
-    return (
-      <div className="searchContainer col-8 p-4 shadow rounded-4">
-        <form className="d-flex" role="search">
-          <input
-            className="searchBar form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-            onChange={(e) => console.log(e.target.value)}
-          />
-          <button className="search-btn" type="submit">
-            Search
-          </button>
-        </form>
-      </div>
-    );
-  };
-
-  const Item = ({ img, title, desc, price, btnTitle, btnColor }) => {
-    return (
-      <div className="col-md-4 mb-4">
-        <div
-          className="card shadow"
-          style={{ width: "18rem", height: "30rem" }}
-        >
-          <div className="card-body">
-            <ItemImage img={img} />
-            <Title title={title} />
-            <Description desc={desc} />
-            <Price price={price} />
-          </div>
-          <div className="card-footer p-2 bg-transparent">
-            <div className="d-flex justify-content-around">
-              <Button
-                className="fixed-bottom"
-                btnTitle={btnTitle}
-                btnColor={btnColor}
-              />
-              <Button
-                className="fixedbottom"
-                btnTitle="Add to cart"
-                btnColor="btn btn-success"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  };
-
-  const tea_items = [
-    {
-      id: 1,
-      title: "Cinoman Tea",
-      img: "https://i.pinimg.com/736x/73/fe/e2/73fee27cb33a7e40a931f470e47a4752.jpg",
-      desc: "Cinnamon tea made from the fresh tea leaves",
-      price: "Rs. 1000",
-      btnTitle: "Book Now",
-      btnColor: "btn btn-primary",
-    },
-    {
-      id: 2,
-      title: "Green Tea",
-      img: "https://i.pinimg.com/1200x/e3/25/3c/e3253cf6a271674e201e1415c8e77cdc.jpg",
-      desc: "Grean Tea is nice and fresh",
-      price: "Rs. 1500",
-      btnTitle: "Book Now",
-      btnColor: "btn btn-primary",
-    },
-    {
-      id: 3,
-      title: "Orange Tea",
-      img: "https://i.pinimg.com/736x/2d/00/fa/2d00fa9769224e928fdd010b0c98ab9f.jpg",
-      desc: "Fresh Orange tea made from the fresh tea leaves",
-      price: "Rs. 1200",
-      btnTitle: "Book Now",
-      btnColor: "btn btn-primary",
-    },
-    {
-      id: 4,
-      title: "Blue Marigold Tea",
-      img: "https://i.pinimg.com/736x/cf/82/85/cf82857f8fb09b63aec28e7b0671ced6.jpg",
-      desc: "Blue Marigold tea made from the fresh tea leaves",
-      price: "Rs. 1000",
-      btnTitle: "Book Now",
-      btnColor: "btn btn-primary",
-    },
-    {
-      id: 5,
-      title: "cappuccino",
-      img: "https://i.pinimg.com/736x/c9/a3/f2/c9a3f2aebfa384a09f86274471f5c2ea.jpg",
-      desc: "Capaccino made from the fresh tea leaves",
-      price: "Rs. 1800",
-      btnTitle: "Book Now",
-      btnColor: "btn btn-primary",
-    },
-  ];
+  if (loading) return <p className="text-center mt-5">Loading...</p>;
 
   return (
-    <>
-      <div className="container mt-5 pt-5">
-        <div className="d-flex mb-5 justify-content-center">
-          <SearchForm />
-        </div>
-        <div className="row d-flex align-items-justify">
-          {tea_items.map((item) => (
-            <Item
-              img={item.img}
-              title={item.title}
-              desc={item.desc}
-              price={item.price}
-              btnTitle={item.btnTitle}
-              btnColor={item.btnColor}
-              key={item.id}
-            />
-          ))}
-        </div>
+    <div className="container mt-5">
+     <Typography variant="h4"   sx={{flexGrow: 1, fontFamily: 'Poppins', textAlign: 'center', fontWeight: 'bold', color: '#eaeef2ff', mt: 2,}}>
+  All Tea 🍃
+</Typography>
+      <div className="row">
+        {items.length === 0 ? (
+          <p className="text-center">No items found.</p>
+        ) : (
+          items.map((item) => (
+            <div className="card-container col-md-4 mb-4" key={item._id}>
+              <div className="custom-card h-100 shadow-sm">
+                {item.image && (
+                  <img
+                    src={item.image}
+                    alt={item.title}
+                    className="card-image card-img-top"
+                    style={{ objectFit: "cover", height: "200px" }}
+                  />
+                )}
+                <div className="card-body">
+                  <h5 className="card-title">{item.title}</h5>
+                  <p className="card-text">{item.description}</p>
+                  <p className="fw-bold">Rs. {item.price}</p>
+                </div>
+              </div>
+            </div>
+          ))
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,70 +1,219 @@
-import {useState} from "react"
-const Dashboard = () => {
-
-  const [formData, setFormData] = useState({
-    title: '',
-    description: '',
-    price: '',
-    image: null, 
-  });
-
-
-  const TextField = ({placeholder, type}) => {
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import {  AppBar, Toolbar, Typography } from "@mui/material";
+const Button = ({ title, btnColor }) => {
+  return (
+    <button type="submit" className={btnColor}>
+      {title}
+    </button>
+  );
+};
+const TextField = ({ placeholder, type, name, value, handleChange }) => {
+  if (type === "file") {
     return (
       <div className="p-3">
-    <input type={type} 
-    className="form-control" 
-    placeholder={placeholder} 
-    aria-label="Search"/>
+        <label htmlFor={name} className="form-label">
+          {placeholder}
+        </label>
+        <input
+          type="file"
+          name={name}
+          className="form-control"
+          id={name}
+          onChange={handleChange}
+          accept="image/*"
+        />
+
+        {value && typeof value === "object" && (
+          <div className="mt-3">
+            <img
+              src={URL.createObjectURL(value)}
+              alt="Preview"
+              className="img-thumbnail"
+              style={{ maxHeight: "200px" }}
+            />
+          </div>
+        )}
       </div>
     );
+  }
+
+  return (
+    <div className="p-3">
+      <input
+        type={type}
+        name={name}
+        value={value}
+        className="form-control"
+        placeholder={placeholder}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+const ProductTable = ({Productdata}) => {
+  return (
+    <div className="product-table">
+      <table className="table">
+        <thead className="table-header">
+          <tr>
+            <th>Product</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Image</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody className="table-body">
+          {Productdata.map((item) => (
+            <tr key={item._id}>
+              <td>{item.title}</td>
+              <td>{item.description}</td>
+              <td>{item.price}</td>
+              <td>
+                <img
+                  src={item.image}
+                  alt={item.title}
+                  className="img-fluid"
+                  style={{ maxWidth: "100px" }}
+                />
+              </td>
+              <td>
+                <button className="editButton">Edit</button>
+                <button className="deleteButton">Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+};
+
+
+const Dashboard = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    price: "",
+    image: null,
+  });
+
+  const [items, setItem] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/item/getall");
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.message || "Something went wrong on the server");
+        }
+       setItem(data.items);
+       setLoading(false);
+       console.log(data);
+      } catch (err) {
+        console.error("Fetching error:", err);
+        setError(err.message);
+      }
+    };
+    fetchItems();
+  },[]);
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    if (name === "image") {
+      setFormData({ ...formData, image: files[0] });
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
   };
 
-  const Button = ({title, btnColor}) =>{
-    return <button type="submit" className={btnColor}>{title}</button>   
-  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    data.append("title", formData.title);
+    data.append("description", formData.description);
+    data.append("price", formData.price);
+    data.append("image", formData.image);
 
-  const Form = () => {
-    return (
-      <div>
-        <form>
-          <div className="row">
-          <div className="col-12">
-          <TextField placeholder={"Brand Name"} type="text"/>
-          </div>
-          <div className="col-12">
-          <TextField placeholder="Description" type="text"/>
-          </div>
-          <div className="col-12">
-          <TextField placeholder="price" type="number"/>
-          </div>
-          <div className="col-12">
-          <TextField placeholder="Image URL" type="file"/>
-          </div>
-          <div className="col-12 mx-3 mt-2">
-            <Button title="Save Product" btnColor="btn btn-outline-success"/>
-          </div>
-          </div>
-        </form>
-      </div>
-    )
-  }
+    try {
+      const res = await fetch("http://localhost:5000/api/item/create", {
+        method: "POST",
+        body: data,
+      });
 
-  return(
-       <>
+      const result = await res.json();
+
+      if (!res.ok) {
+        throw new Error(result.message || "Something went wrong on the server");
+      }
+
+      alert(result.message);
+      setFormData({ title: "", description: "", price: "", image: null });
+    } catch (error) {
+      console.error("Error from frontend:", error);
+      alert(`Error: ${error.message}`);
+    }
+  };
+
+  return (
     <div className="container mt-5">
-      <div className="card col-8 mx-auto">
-        <div className="card-header bg-dark">
-          <h1 className="text-center text-light text-uppercase card-title fw-light">Add Product</h1>
+      <div className="card col-md-8 col-lg-6 mx-auto">
+        <div className="card-header">
+          <h1 className="text-center card-title">Add New Product</h1>
         </div>
         <div className="card-body">
-          <Form/>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              placeholder="Product Title"
+              type="text"
+              name="title"
+              value={formData.title}
+              handleChange={handleChange}
+            />
+            <TextField
+              placeholder="Description"
+              type="text"
+              name="description"
+              value={formData.description}
+              handleChange={handleChange}
+            />
+            <TextField
+              placeholder="Price"
+              type="number"
+              name="price"
+              value={formData.price}
+              handleChange={handleChange}
+            />
+            <TextField
+              placeholder="Product Image"
+              type="file"
+              name="image"
+              value={formData.image}
+              handleChange={handleChange}
+            />
+            <div className="p-3">
+              <Button title="Save Product" btnColor="btn btn-primary" />
+            </div>
+          </form>
         </div>
       </div>
+      <div className="card mt-4 mb-4">
+      <div className="card-header">
+         <Typography variant="h4"   sx={{flexGrow: 1, fontFamily: 'Poppins', textAlign: 'center', fontWeight: 'bold', color: '#2e11e8ff', mt: 2,}}>
+  All Tea 🍃
+</Typography>
+      </div>
+      <div className="card-body">
+        <ProductTable Productdata={items}/>
+      </div>
+      </div>
     </div>
-    </>
+  );
+};
 
-  )
-}
-
-export default Dashboard
+export default Dashboard;
